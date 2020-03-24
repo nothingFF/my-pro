e78d6aef5f921d9c73c9ebc34f7ba2c5c682ed83
林上晓 <shangxiao.lin@keystore.com>
2020-03-24 10:59:20 +0800

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    {{info}}
+  </div>
+</template>
+
+<script>
+
+import Axios from 'axios'
+
+export default {
+  name: 'test',
+  data () {
+    return {
+      info: 'Welcome to Your Vue.js App'
+    }
+  },
+  mounted () {
+    Axios
+      .get('https://www.runoob.com/try/ajax/json_demo.json')
+      .then(response => (this.info = response))
+      .catch(function (error) { // 请求失败处理
+        console.log(error)
+      })
+  }
+}
+</script>
